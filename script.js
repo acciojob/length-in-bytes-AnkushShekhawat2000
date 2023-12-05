fe30bf3050a1fd9b96f94eb2f6198edb23518a7e
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+// const byteSize = (str) => {
+//   // write your code here
+// };
+
+// // Do not change the code below
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
+
+
+
 const byteSize = (str) => {
-  // write your code here
+  // Create a Blob object with the given string
+  const blob = new Blob([str]);
+
+  // Get the size of the Blob, which represents the byte size of the string
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
-// Do not change the code below
+// Example
 const str = prompt("Enter some string.");
 alert(byteSize(str));
